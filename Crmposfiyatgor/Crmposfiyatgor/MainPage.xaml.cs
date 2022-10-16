57e2e45feaf327cd816a4bcf81222e59b83ffc62
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2022-10-16 13:00:44 +0300

    Proje Created

---
+++ Crmposfiyatgor/Crmposfiyatgor/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Data.SqlClient;
+
+namespace Crmposfiyatgor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            barkod.Focus();
+            
+        }
+        SqlConnection sqlConnection;
+        public void baglanti()
+        {
+            var sqladresvalue = Application.Current.Properties["sqladres"];
+            var sqldbadvalue = Application.Current.Properties["sqldb"];
+            var sqlkulvalue = Application.Current.Properties["sqlkul"];
+            var sqlpwvalue = Application.Current.Properties["sqlpw"];
+            string sqlconn = $"Data Source={sqladresvalue};Initial Catalog={sqldbadvalue};User ID={sqlkulvalue};Password={sqlpwvalue}";
+
+            sqlConnection = new SqlConnection(sqlconn);
+        }
+
+        private async void barkod_Completed(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti();
+                sqlConnection.Open();
+
+                SqlCommand command = new SqlCommand($"SELECT * FROM dbo.wwwFIYAT_GOR WHERE BARKOD = '{barkod.Text.Trim()}' ", sqlConnection);
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    stokadi.Text = dr["AD"].ToString();
+                    tlfiyat.Text = dr["FIYAT"].ToString() + " TL";
+                    dolarfiyat.Text = dr["DOLARFIYAT"].ToString() + " $";
+                    eurofiyat.Text = dr["EUROFIYAT"].ToString() + " €";
+                   // lblmiktar.Text = dr["MIKTAR"].ToString();
+                }
+
+                dr.Close();
+                sqlConnection.Close();
+                barkod.Focus();
+                barkod.Text = " ";
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "Ok");
+                throw;
+            }
+
+        }
+
+        private async void btnsqlayar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new sqlayar());
+        }
+    }
+}
---
+++ Crmposfiyatgor/Crmposfiyatgor/sqlayar.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Crmposfiyatgor
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class sqlayar : ContentPage
+    {
+        public sqlayar()
+        {
+            InitializeComponent();
+            
+        }
+
+        public string sqlconn2;
+        private SqlConnection sqlConnection;
+        private async void concontrol_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                sqladres.Text = Application.Current.Properties["sqladres"].ToString();
+                sqldbad.Text = Application.Current.Properties["sqldb"].ToString();
+                sqlkul.Text = Application.Current.Properties["sqlkul"].ToString();
+                sqlpw.Text = Application.Current.Properties["sqlpw"].ToString();
+                sqlconn2 = $"Data Source={sqladres.Text};Initial Catalog={sqldbad.Text};User ID={sqlkul.Text};Password={sqlpw.Text}";
+                sqlConnection = new SqlConnection(sqlconn2);
+                sqlConnection.Open();
+                await App.Current.MainPage.DisplayAlert("İnfo", "Bağlantı Sağlandı", "Ok");
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "Ok");
+                throw;
+            }
+
+        }
+
+        private async void consave_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["sqladres"] = sqladres.Text;
+            Application.Current.Properties["sqldb"] = sqldbad.Text;
+            Application.Current.Properties["sqlkul"] = sqlkul.Text;
+            Application.Current.Properties["sqlpw"] = sqlpw.Text;
+
+            await Application.Current.SavePropertiesAsync();
+            await App.Current.MainPage.DisplayAlert("info", "Kayıt Başarılı", "Ok");
+
+        }
+    }
+}
